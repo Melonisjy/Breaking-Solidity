b01898587f30611a76b7e4e3f1fbc9be2d2040d0
melonisjy <dhy2457@naver.com>
2023-02-14 17:09:41 +0900

    initial commit

---
+++ lecture/lec3.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract lec3 {
+    // 1 ether = 10^9 Gwei = 10^18 wei
+    // 0.00000000000000001 ether = 1^-18 = 1 wei
+    // 0.01 ehter = 10^16wei
+
+    uint256 public value = 1 ether;
+    uint256 public value2 = 1 wei;
+    uint256 public value3 = 1 gwei;
+}
---
+++ lecture/lec4.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract lec4 {
+
+    uint256 public a = 3;
+    // 1. parameter와 return 값이 없는 function
+    function changeA1() public {
+        a = 5;
+    }
+
+    // 2. parameter는 있고, return 값이 없는 function
+    function changeA2(uint256 _value) public {
+        a = _value;
+    }
+
+    // 3. parameter와 return 값이 있는 function
+    function changeA3(uint256 _value) public returns(uint256) {
+        a = _value;
+        return a;
+    }
+}
