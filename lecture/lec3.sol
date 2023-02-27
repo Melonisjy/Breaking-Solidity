79bb8c13bce82b3bfa7d585a73e748d236cb3495
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2023-02-27 23:47:56 +0900

    3강 - Gas와 Ether의 단위

--- lecture/lec3.sol
+++ lecture/lec3.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >= 0.7.0 < 0.9.0;
 
+// 3강 - Gas와 Ether의 단위
 contract lec3 {
     // 1 ether = 10^9 Gwei = 10^18 wei
     // 0.00000000000000001 ether = 1^-18 = 1 wei
