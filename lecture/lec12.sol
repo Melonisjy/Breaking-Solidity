5cf24d69bf250edc0a71098acc60cec5e7d1d30f
Lee Joo Yeol <79883658+Melonisjy@users.noreply.github.com>
2023-02-27 23:53:07 +0900

    12강 - 상속3 - 두개 이상 상속하기

--- lecture/lec12.sol
+++ lecture/lec12.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >= 0.7.0 < 0.9.0;
 
+// 12강 - 상속3 - 두개 이상 상속하기
 contract Father {
     uint256 public fatherMoney = 100;
     function getFatherName() public pure returns(string memory){
